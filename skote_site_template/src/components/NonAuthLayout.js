e8ca6272fd6212c9706f30e257394a6390cdb5f1
Carl Hewitt <carl@decisions.com>
2020-11-27 17:26:33 -0500

    made copy of template for reference.

---
+++ skote_site_template/src/components/NonAuthLayout.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { withRouter } from "react-router-dom";
+import {NonAuthLayout} from "@appglue/ui";
+
+// class NonAuthLayout extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state={};
+//         this.capitalizeFirstLetter.bind(this);
+//     }
+//
+//     capitalizeFirstLetter = string => {
+//         return string.charAt(1).toUpperCase() + string.slice(2);
+//       };
+//
+//     componentDidMount(){
+//         let currentage = this.capitalizeFirstLetter(this.props.location.pathname);
+//
+//         document.title =
+//           currentage + " | Skote - Responsive Bootstrap 4 Admin Dashboard";
+//     }
+//     render() {
+//         return <React.Fragment>
+//             {this.props.children}
+//         </React.Fragment>;
+//     }
+// }
+
+export default (withRouter(NonAuthLayout));
