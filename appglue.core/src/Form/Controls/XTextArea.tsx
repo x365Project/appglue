b1bc7fe447b3d9e7b1ddcc832a94eea0a8ec6259
Ahmed Soror <devsolutions570@gmail.com>
2020-11-29 19:14:43 +0100

    Add hint text to XTextArea

--- appglue.core/src/Form/Controls/XTextArea.tsx
+++ appglue.core/src/Form/Controls/XTextArea.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import styled from 'styled-components';
 import {RegisterUIControl, ControlType} from '../Utilities/RegisterUIControl';
 import {TextField, TextareaAutosize} from '@material-ui/core';
 import {BaseTextEntryControl} from "./BaseTextEntryControl";
 import {TextAreaIcon} from "../../CommonUI/Icon/TextAreaIcon";
+import FormHelperText from '@material-ui/core/FormHelperText';
 
 
 @RegisterUIControl('Data (Entry)', 'Text Area', ControlType.Control, <TextAreaIcon />)
@@ -12,15 +14,20 @@ export class XTextArea extends BaseTextEntryControl {
 
 	render() {
 		return (
-			<TextareaAutosize
-				data-testid={this.valueName}
-				style={{width: '100%'}}
-				rowsMax={this.rowMax}
-				rowsMin={this.rowMin}
-				placeholder={this.placeholderText}
-				onChange={this.handleChange}
-				value={(this.valueName)?this.getFormDataValue(this.valueName):''}
-			/>
+			<>
+				<TextareaAutosize
+					data-testid={this.valueName}
+					style={{width: '100%'}}
+					rowsMax={this.rowMax}
+					rowsMin={this.rowMin}
+					placeholder={this.placeholderText}
+					onChange={this.handleChange}
+					value={(this.valueName)?this.getFormDataValue(this.valueName):''}
+				/>
+				{this.hintText && (
+					<StyledFormHelperText>{this.hintText}</StyledFormHelperText>
+				)}
+			</>
 		)
 	}
 
@@ -58,3 +65,12 @@ class XTextAreaEditUI extends React.Component<{editMe:XTextArea}> {
         );
     }
 }
+
+const StyledFormHelperText = styled(FormHelperText) `
+	font-family: Mulish !important;
+	font-weight: 600 !important;
+	font-size: 12px !important;
+	line-height: 15px !important;
+	margin-top: 6px !important;
+	color: #677C95 !important;
+`
