8a9263044a8a382a2ecd155d479ad7788721d244
brunodev11 <bweur123@gmail.com>
2020-12-15 11:24:13 -0300

    - working

---
+++ appglue.core/src/Expressions/ExpressionElements/index.tsx
@@ -0,0 +1,44 @@
+
+
+// logic
+import * as IfthenExpression from "./Logic/IfThenExpression";
+import {ExistsExpression} from "./Logic/Exists";
+import {GreaterThanExpression} from "./Logic/GreaterThan";
+import {GreaterThanEqualToExpression} from "./Logic/GreaterThanEqualTo";
+import {AndGroup} from "./Logic/Grouping";
+import {OrGroup} from "./Logic/Grouping";
+import {IsFalseExpression} from "./Logic/IsFalse";
+import {IsTrueExpression} from "./Logic/IsTrue";
+import {LessThanExpression} from "./Logic/LessThan";
+import {LessThanEqualToExpression} from "./Logic/LessThanEqualTo";
+
+
+// math
+import {AddExpression} from "./Math/AddExpression";
+import {DivideExpression} from "./Math/DivideExpression";
+import {MultiplyExpression} from "./Math/MultiplyExpression";
+import {SumExpression} from "./Math/SumExpression";
+
+// string
+import {StringLengthExpression} from "./String/StringLengthExpression";
+
+
+export {
+	...IfthenExpression,
+	ExistsExpression,
+	GreaterThanExpression,
+	GreaterThanEqualToExpression,
+	AndGroup,
+	OrGroup,
+	IsFalseExpression,
+	IsTrueExpression,
+	LessThanExpression,
+	LessThanEqualToExpression,
+
+	AddExpression,
+	DivideExpression,
+	MultiplyExpression,
+	SumExpression,
+
+	StringLengthExpression,
+}
