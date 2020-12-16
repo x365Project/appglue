cca97be85780a70e3b9b32a82810f61ba070a2e6
brunodev11 <bweur123@gmail.com>
2020-12-16 06:31:08 -0300

    - add property textlist

---
+++ appglue.core/src/CommonUI/PropertyEditing/PropertyEditorTextList.stories.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import {Meta, Story} from "@storybook/react/types-6-0";
+import {XFormDesigner} from "../../Form/XFormDesigner";
+import {PropertyEditorTextList, PropertyEditorTextListInterface} from "./PropertyEditorTextList";
+import { ObserveState } from "../StateManagement/ObserveState";
+import { StateManager } from "../StateManagement/StateManager";
+
+
+
+export default {
+    title: "Shared/Property Editors",
+    component: XFormDesigner,
+} as Meta;
+
+
+const MissingTemplate: Story<{}> = () => (
+    <div>
+        Missing
+    </div>
+);
+
+
+class TextListData implements PropertyEditorTextListInterface {
+    editObject: object = {};
+    label: string = 'TextEditor';
+    propertyName: string = 'textList';
+
+    updateCallback: () => void = () => {
+        StateManager.propertyChanged(this, this.propertyName)
+    }
+
+}
+
+let testData = new TextListData();
+
+const TextListTemplate: Story<PropertyEditorTextListInterface> = 
+    (args) => <ObserveState listenTo={testData} properties={[testData.propertyName]} control={() => <PropertyEditorTextList {...args} />} />;
+
+export const TextListEditor = TextListTemplate.bind({}, testData);
--- appglue.core/src/CommonUI/PropertyEditing/PropertyEditorTextList.tsx
+++ appglue.core/src/CommonUI/PropertyEditing/PropertyEditorTextList.tsx
@@ -1,34 +1,118 @@
-import TextField from "@material-ui/core/TextField"
-import React from "react";
-import { Input } from "@material-ui/core";
+import React, { useState } from "react";
+import { Input, InputLabel, ClickAwayListener } from "@material-ui/core";
+
+import styled from "styled-components";
+
+const StyledTextList = styled("div")<{
+    active: boolean;
+    label?: string;
+    empty: boolean;
+}>`
+    width: 100%;
+
+    .TextList-TextBox {
+        display: flex;
+        flex-direction: column;
+        border: solid 1px ${props => props.active ? '#93A9BF' :'#D8E4EE'};
+        border-radius: 4px;
+        font-family: Mulish;
+        padding: 6px 12px;
+    
+        transition: all .3s;
+
+        .MuiInputBase-input {
+            color: ${props => props.active ? '#677C95': '#93A9BF'};
+            font-family: Mulish;
+            font-style: normal;
+            font-weight: 600;
+            font-size: 14px;
+            line-height: 20px;
+            padding: 0;
+        }
+    }
+
+    .TextList-Label {
+        color: #677C95;
+        font-family: Mulish;
+        font-style: normal;
+        font-weight: 600;
+        font-size: 14px;
+        line-height: 20px;
+        ${props => props.empty && `
+            transform: translate(10px, 24px);
+        `}
+    }
+
+`;
 
 export interface PropertyEditorTextListInterface{
     editObject: object,
     propertyName: string | number,
     label?: string,
-    updateCallback : CallableFunction,
+    updateCallback : () => void,
 }
+
 
 export const PropertyEditorTextList : React.FC<PropertyEditorTextListInterface> = (props: PropertyEditorTextListInterface) => {
 
-    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        Reflect.set(props.editObject, props.propertyName, event.target.value);
+    const [selectedLine, setSelectedLine] = useState<number>(-1);
+    const values = Reflect.get(props.editObject, props.propertyName) || [];
+
+
+    const update = (vals: string[]) => {
+        Reflect.set(props.editObject, props.propertyName, vals);
         props.updateCallback();
     }
-    const value = Reflect.get(props.editObject, props.propertyName) || [];
+
+
+    if (values.length === 0) {
+        values.push('')
+        update(values);
+    }
+
+
+    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (selectedLine >= 0) {
+            values[selectedLine] = event.target.value;
+            update(values);
+        }
+    }
+
+    const onKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.keyCode === 13) {
+            if (selectedLine === values.length - 1) {
+                values.push('');
+                update(values);
+            }
+            setSelectedLine(selectedLine + 1);
+        }
+    }
+
+    const onFocus = (index: number) => {
+        setSelectedLine(index);
+    }
+
+    const onClickAway = () => {
+        setSelectedLine(-1);
+    }
+
     return (
-        <>
-            {
-                props.label && (
-                    <TextField variant="outlined" value={value || ''} label={props.label} onChange={onChange} />
-                )
-            }
-            {
-                !props.label && (
-                    <Input value={value || ''} onChange={onChange} />
-                )
-            }
-        </>
+        <ClickAwayListener onClickAway={onClickAway}>
+            <StyledTextList className="TextList-Wrapper" active={selectedLine !== -1} label={props.label} empty={values.length === 1 && values[0] === ''}>
+                {
+                    props.label && (
+                        <InputLabel classes={{root: 'TextList-Label'}}>{props.label}</InputLabel>
+                    )
+                }
+                <div className="TextList-TextBox">
+                {
+                    values && values.map((val: string, idx: number) => (
+                        <Input value={val} key={idx} onChange={onChange} disableUnderline onFocus={() => onFocus(idx)} onKeyDown={onKeyDown} autoFocus={idx === selectedLine} />
+                    ))
+                }
+                </div>
+            </StyledTextList>
+        </ClickAwayListener>
     );
 }
 
